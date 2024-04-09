97fcb27b3060378ab662bf23ce3a133048d3bb21
ivrusson <ivrusson@gmail.com>
2024-04-09 08:42:44 +0200

    feat: :zap: add all providers to testst providers wrapper

--- tests/AllProviders.tsx
+++ tests/AllProviders.tsx
@@ -1,6 +1,24 @@
-import { Theme } from "@radix-ui/themes"
-import { PropsWithChildren } from "react"
+import { PropsWithChildren } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { CartProvider } from "../src/providers/CartProvider";
+import { Theme } from "@radix-ui/themes";
 
-export const AllProviders = ({children}: PropsWithChildren) => {
-  return <Theme>{children}</Theme>;
+const AllProviders = ({ children }: PropsWithChildren) => {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
+  return (
+    <QueryClientProvider client={client}>
+      <CartProvider>
+        <Theme>{children}</Theme>
+      </CartProvider>
+    </QueryClientProvider>
+  );
 };
+
+export default AllProviders;
